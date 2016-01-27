debf8cc1822970faec3d35a0c16c686dab7787a8
Pavelgyravel <pavelgyravel@gmail.com>
2016-01-27 12:40:50 +0200

    Some layout changes

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -19,6 +19,26 @@
     
   </head>
   <body>
+  <nav class="navbar navbar-default " role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="{{URL::to('/')}}">Home</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+              
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="/auth/login">Login</a></li>
+                <li><a href="/auth/register">Register</a></li>
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div>
+        </nav>
    
     <div class="auth_form">
     	<div class="container-fluid ">
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,7 +19,7 @@
     
   </head>
   <body>
-    <nav class="navbar navbar-inverse " role="navigation">
+    <nav class="navbar navbar-default" role="navigation">
           <div class="container">
             <div class="navbar-header">
               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
